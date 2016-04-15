b4e149efce18585f962cc250d2ce7950a0931b7a
anushkamahesh <anushkamahesh@bitbucket.org>
2016-04-16 00:46:28 +0530

    error handling in form

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -17,7 +17,8 @@ angular
 
     .state('register', {
       url: '/register',
-      templateUrl: '/views/register.html'
+      templateUrl: '/views/register.html',
+      controller: 'RegisterCtrl'
     });
 
 });
---
+++ app/scripts/controllers/register.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jwtApp.controller:RegisterCtrl
+ * @description
+ * # RegisterCtrl
+ * Controller of the jwtApp
+ */
+angular.module('jwtApp')
+  .controller('RegisterCtrl', function ($scope) {
+    $scope.submit = function () {
+      console.log('Test');
+    };
+  });
