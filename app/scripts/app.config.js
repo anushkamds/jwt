de0da8d83b0f41da727e0c0e991c9ceb5cb3f3c0
anushkamahesh <anushkamahesh@bitbucket.org>
2016-04-15 21:55:11 +0530

    adding new views and app configuration

---
+++ app/scripts/app.config.js
@@ -0,0 +1,23 @@
+/**
+ * Created by anushkamahesh on 4/15/16.
+ */
+'use strict';
+
+angular
+  .module('jwtApp').config(function ($urlRouterProvider, $stateProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+
+    .state('main', {
+      url: '/',
+      templateUrl: '/views/main.html'
+    })
+
+    .state('register', {
+      url: '/register',
+      templateUrl: '/views/register.html'
+    });
+
+});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,4 @@
  * Main module of the application.
  */
 angular
-  .module('jwtApp', []);
+  .module('jwtApp', ['ui.router']);
