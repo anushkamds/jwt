81ecc4ef2601ca2c4bac4489ee9c1e75e2fc2d0f
anushkamahesh <anushkamahesh@bitbucket.org>
2016-04-16 22:34:47 +0530

    server is added with saving in mongo db

--- server/api.js
+++ server/api.js
@@ -2,13 +2,41 @@
  * Created by anushkamahesh on 4/16/16.
  */
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,UPDATE,PUT,POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  next();
+});
+
+var User = mongoose.model('User', {
+  email: String,
+  password: String
+});
 
 app.post('/register', function (req, res) {
-  res.send('hi');
+  var user = req.body;
+
+  var newUser = new User({
+    email: user.email,
+    password: user.password
+  });
+
+  newUser.save(function (err) {
+    res.status(200).json(newUser);
+  });
+
 });
-  
+
+mongoose.connect('mongodb://127.0.0.1:27017/psjwt');
+
 var server = app.listen(3000, function () {
   console.log('app listening', server.address().port);
 });
